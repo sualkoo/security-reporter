783ce9e4d46d46512d2317885ebf1bff6578d38b
Ciz, Frantisek <frantisek.ciz@siemens-healthineers.com>
2023-06-28 14:30:38 +0200

    Added material

--- SecurityReporter/angularapp/src/app/app.module.ts
+++ SecurityReporter/angularapp/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AppComponent } from './app.component';
+import { ProjectManagerComponent } from './project-manager/project-manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectManagerComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ SecurityReporter/angularapp/src/app/project-manager/project-manager.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-manager',
+  templateUrl: './project-manager.component.html',
+  styleUrls: ['./project-manager.component.css']
+})
+export class ProjectManagerComponent {
+
+}
