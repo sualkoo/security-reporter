b0f69c244b11189eca3701685a8d754264f5e0d4
Martin Laus <mato.laus33@gmail.com>
2023-07-12 13:07:53 +0200

    Renaming of files and folders

--- SecurityReporter/angularapp/src/app/app.module.ts
+++ SecurityReporter/angularapp/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AppComponent } from './app.component';
-import { ProjectManagementComponent } from './bratislava/project-management/project-management.component';
+import { AddProjectComponent } from './project-management/component-pages/add-project-page/add-project.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -12,10 +12,10 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { NgFor } from '@angular/common';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
-import { DatepickerComponent } from 'src/app/bratislava/components/datepicker-component/datepicker-component.component';
-import { SelectComponentComponent } from './bratislava/components/select-component/select-component.component';
-import { InputComponentComponent } from './bratislava/components/input-component/input-component.component';
-import { RadioButtonComponentComponent } from './bratislava/components/radio-button-component/radio-button-component.component';
+import { DatepickerComponent } from 'src/app/project-management/components/datepicker-component/datepicker-component.component';
+import { SelectComponentComponent } from './project-management/components/select-component/select-component.component';
+import { InputComponentComponent } from './project-management/components/input-component/input-component.component';
+import { RadioButtonComponentComponent } from './project-management/components/radio-button-component/radio-button-component.component';
 
 @NgModule({
   declarations: [AppComponent],
@@ -31,7 +31,7 @@ import { RadioButtonComponentComponent } from './bratislava/components/radio-but
     NgFor,
     MatSelectModule,
     MatRadioModule,
-    ProjectManagementComponent,
+    AddProjectComponent,
     DatepickerComponent,
     SelectComponentComponent,
     InputComponentComponent,
