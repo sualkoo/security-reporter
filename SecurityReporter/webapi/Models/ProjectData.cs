b57eed003ee0193e216b7603db4952c1777527dd
Martin Kubička <martinkubicka22@gmail.com>
2023-07-12 14:19:54 +0200

    Add enum validation

--- SecurityReporter/webapi/Models/ProjectData.cs
+++ SecurityReporter/webapi/Models/ProjectData.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using webapi.Enums;
 
 namespace webapi.Models;
@@ -6,9 +7,17 @@ public class ProjectData
 {
     public Guid id { get; set; }
     public string ProjectName { get; set; }
+    
+    [Range(0, 5, ErrorMessage = "Value for attribute {0} must be between {1} and {2}.")]
     public ProjectStatus? ProjectStatus { get; set; }
+
+    [Range(0, 2, ErrorMessage = "Value for attribute {0} must be between {1} and {2}.")]
     public ProjectQuestionare? ProjectQuestionare { get; set; }
+
+    [Range(0, 3, ErrorMessage = "Value for attribute {0} must be between {1} and {2}.")]
     public ProjectScope? ProjectScope { get; set; }
+
+    [Range(0, 1, ErrorMessage = "Value for attribute {0} must be between {1} and {2}.")]
     public ProjectDuration? ProjectDuration { get; set; }
     public DateOnly? StartDate { get; set; }
     public DateOnly? EndDate { get; set; }
@@ -17,6 +26,8 @@ public class ProjectData
     public DateOnly RequestCreated { get; set; }
     public List<Comment>? Commments { get; set; }
     public string? CatsNumber { get; set; }
+
+    [Range(0, 9, ErrorMessage = "Value for attribute {0} must be between {1} and {2}.")]
     public ProjectOfferStatus? ProjectOfferStatus { get; set; }
     public string? PentestAspects { get; set; }
     public List<string>? WorkingTeam { get; set; }
