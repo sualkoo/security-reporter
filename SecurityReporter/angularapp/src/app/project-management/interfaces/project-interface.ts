98e0364b23a2a61b63cc1f51f0de6f296e799664
Kamila Habalova <kamilahabalova747@gmail.com>
2023-07-13 14:55:40 +0200

    Create service for adding project

---
+++ SecurityReporter/angularapp/src/app/project-management/interfaces/project-interface.ts
@@ -0,0 +1,48 @@
+export interface ProjectInterface {
+  id: string;
+  ProjectName?: string;
+  StartDate?: Date;
+  EndDate?: Date;
+  ProjectStatus?: ProjectStatus;
+  ProjectScope?: ProjectScope;
+  ProjectQuestionare?: ProjectQuestionare;
+  PentestAspects?: string;
+  PentestDuration?: number;
+  ReportDueDate?: Date;
+  IKO?: Date;
+  TKO?: Date;
+  RequestCreated?: string;
+  Commments: string;
+  CatsNumber?: string;
+  ProjectOfferStatus?: ProjectOfferStatus;
+  WorkingTeam: string[];
+  ProjectLead?: string;
+  ReportStatus?: string;
+  ContactForClients: string[];
+}
+
+type ProjectStatus =
+  | 'TBS'
+  | 'Requested'
+  | 'Planned'
+  | 'In progress'
+  | 'Finished'
+  | 'Cancelled'
+  | 'On hold';
+
+type ProjectQuestionare = 'TBS' | 'Sent' | 'Received';
+
+type ProjectScope = 'TBS' | 'Sent' | 'Confirmed' | 'Signed';
+
+type ProjectOfferStatus =
+  | 'TBS'
+  | 'Waiting for Offer creation'
+  | 'Offer Draft sent for Review'
+  | 'Offer sent for signatue'
+  | 'Offer signed - Ready For Invoicing'
+  | 'Invoiced'
+  | 'Individual Agreement'
+  | 'Retest - free of charge'
+  | 'Other'
+  | 'Cancelled'
+  | 'Prepared';
