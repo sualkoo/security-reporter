1d90ed3de126aba61bd5d42579917cf1e8464d2d
Petra Miková <petramikova063@gmail.com>
2023-07-13 13:59:54 +0200

    Resolve merge

---
+++ SecurityReporter/webapi/ProjectManagement/Utils/EmailListValidationAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class EmailListValidationAttribute : ValidationAttribute
+{
+    public override bool IsValid(object value)
+    {
+        if (value is List<string> stringList)
+        {
+            if (stringList.Count == 0)
+            {
+                return false;
+            }
+
+            return stringList.All(s => IsValidEmail(s));
+        }
+
+        return true;
+    }
+
+    private bool IsValidEmail(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return false;
+        }
+
+        return new EmailAddressAttribute().IsValid(value);
+    }
+}
+
+
---
+++ SecurityReporter/webapiTests1/webapiTests1/ProjectManagement/Utils/CommentValidationAttributeTests.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+using webapi.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using webapi.Models;
+
+namespace webapi.Utils.Tests
+{
+    [TestFixture]
+    public class CommentValidationAttributeTests
+    {
+        [Test]
+        public void IsValid_CommentListWithNullComment_ValidationFails()
+        {
+            // Arrange
+            var attribute = new CommentValidationAttribute();
+            var commentlList = new List<Comment> {
+                new Comment
+                {
+                    Author = "John Doe",
+                    Text = "This is a random comment.",
+                    CreatedAt = DateTime.Now
+                },
+                null
+            };
+
+            // Act
+            var isValid = attribute.IsValid(commentlList);
+
+            // Assert
+            Assert.IsFalse(isValid);
+        }
+
+        [Test]
+        public void IsValid_EmptyCommentList_ValidationFails()
+        {
+            // Arrange
+            var attribute = new CommentValidationAttribute();
+            var commentlList = new List<Comment> {};
+
+            // Act
+            var isValid = attribute.IsValid(commentlList);
+
+            // Assert
+            Assert.IsFalse(isValid);
+        }
+
+        [Test]
+        public void IsValid_ValidCommentList_ValidationPasses()
+        {
+            // Arrange
+            var attribute = new CommentValidationAttribute();
+            var commentlList = new List<Comment> {
+                new Comment
+                {
+                    Author = "John Doe",
+                    Text = "This is a random comment.",
+                    CreatedAt = DateTime.Now
+                },
+                new Comment
+                {
+                    Author = "Martin",
+                    Text = "This is a random comment.",
+                    CreatedAt = DateTime.Now
+                }
+            };
+
+            // Act
+            var isValid = attribute.IsValid(commentlList);
+
+            // Assert
+            Assert.IsTrue(isValid);
+        }
+
+        [Test]
+        public void IsValid_NullCommentList_ValidationPasses()
+        {
+            // Arrange
+            var attribute = new CommentValidationAttribute();
+            List<Comment> commentlList = null;
+
+            // Act
+            var isValid = attribute.IsValid(commentlList);
+
+            // Assert
+            Assert.IsTrue(isValid);
+        }
+    }
+}
