cb828ce9e2356f0a7972f11b96ab607685fae1b2
Radoslav Žilka <radoslavZilka@outlook.com>
2023-07-14 10:11:13 +0200

    Added Document_Information parser functionality

--- SecurityReporter/webapi/ProjectSearch/Controllers/ProjectReportController.cs
+++ SecurityReporter/webapi/ProjectSearch/Controllers/ProjectReportController.cs
@@ -1,17 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using webapi.ProjectSearch.Models;
+using webapi.ProjectSearch.Services;
 
 namespace webapi.ProjectSearch.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class ProjectReportController : Controller
-    {
+    public class ProjectReportController : Controller { 
+        public IProjectDataParser ProjectDataParser { get; set; }
+        public ProjectReportController (IProjectDataParser parser ) {
+            ProjectDataParser = parser;
+        }
+    
         [HttpPost("add")]
         public async Task<IActionResult> addProjectReport(IFormFile file)
         {
-
+            ProjectReportData newReportData = ProjectDataParser.Extract(file.OpenReadStream());
 
             // Process or save the uploaded file as needed
+            
 
             return Ok("File uploaded successfully.");
         }
