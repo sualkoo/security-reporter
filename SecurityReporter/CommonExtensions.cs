571f16fd2808cbb0dc39a1cd04eb3d071d95370f
Martin Laus <mato.laus33@gmail.com>
2023-07-17 10:12:26 +0200

    create directories for cli template

---
+++ SecurityReporter/CommonExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace System
+{
+    public static class StringExtensions
+    {
+        public static string Reverse(this string value)
+        {
+            var tempArray = value.ToCharArray();
+            Array.Reverse(tempArray);
+            return new string(tempArray);
+        }
+    }
+}
