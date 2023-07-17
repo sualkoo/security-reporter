2d0023df0b3c83077cad14800cce589ddcc131a2
Petra Miková <petramikova063@gmail.com>
2023-07-17 10:00:51 +0200

    Create new page and add routing

---
+++ SecurityReporter/angularapp/src/app/project-listing/component-pages/list-projects-page/list-projects-page.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListProjectsPageComponent } from './list-projects-page.component';
+
+describe('ListProjectsPageComponent', () => {
+  let component: ListProjectsPageComponent;
+  let fixture: ComponentFixture<ListProjectsPageComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ListProjectsPageComponent]
+    });
+    fixture = TestBed.createComponent(ListProjectsPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
