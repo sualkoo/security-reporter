f10ff2bddc93834f82593e8155c9b37a69350368
Petra Miková <petramikova063@gmail.com>
2023-07-17 14:50:30 +0200

    Create service for getting projects into FE

---
+++ SecurityReporter/angularapp/src/app/project-listing/services/get-projects-service.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetProjectsServiceService {
+
+  private getProjectEndPointURL: string;
+
+  constructor(private http: HttpClient) {
+    this.getProjectEndPointURL = 'https://localhost:7075/Project/retrieve';
+  }
+
+  public getProjects(pageSize: number, pageNumber: number): Promise<any> {
+    this.getProjectEndPointURL = this.getProjectEndPointURL + '?pageSize=' + pageSize + '&pageNumber=' + pageNumber;
+    return new Promise((resolve, reject) => {
+      this.http.get(this.getProjectEndPointURL).subscribe(
+        (response) => {
+          resolve(response);
+        },
+        (error) => {
+          console.error(error);
+          reject(error);
+        }
+      );
+    });
+  }
+}
