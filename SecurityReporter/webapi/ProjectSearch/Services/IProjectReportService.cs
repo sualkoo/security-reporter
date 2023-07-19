3d9a117eb2baa4a644d7f1f532bbfc087e028cc5
Martin Lipták <12054222-katpil320@users.noreply.gitlab.com>
2023-07-19 13:26:59 +0200

    Updated project report service and controller to support pagination

--- SecurityReporter/webapi/ProjectSearch/Services/IProjectReportService.cs
+++ SecurityReporter/webapi/ProjectSearch/Services/IProjectReportService.cs
@@ -5,7 +5,7 @@ namespace webapi.ProjectSearch.Services
     public interface IProjectReportService
     {
         public Task<ProjectReportData> GetReportByIdAsync(Guid id);
-        public Task<List<ProjectReportData>> GetReportsAsync(string? subcategory, string keyword, string value);
+        public Task<PagedDBResults<List<ProjectReportData>>> GetReportsAsync(string? subcategory, string keyword, string value, int page);
         public Task<ProjectReportData> SaveReportFromZip(IFormFile file);
     }
 }
