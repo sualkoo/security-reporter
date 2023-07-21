e6477483546c96b487bc6c878d5c399de1eadc9f
Martin Lipták <12054222-katpil320@users.noreply.gitlab.com>
2023-07-21 09:03:46 +0200

    Fixed and improved scrolling, added logic to reset search if user wants to search something else, removed delays while fetching data, removed console logs

--- SecurityReporter/angularapp/src/app/project-search/component-pages/project-search-page/project-search-page.component.ts
+++ SecurityReporter/angularapp/src/app/project-search/component-pages/project-search-page/project-search-page.component.ts
@@ -3,15 +3,33 @@ import { ProjectReportService } from '../../providers/project-report-service';
 import { ProjectDataReport } from '../../interfaces/project-data-report.model';
 import { NotificationService } from '../../providers/notification.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { delay } from 'rxjs';
+import { delay, fromEvent, throttle, throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-project-search',
   templateUrl: './project-search-page.component.html',
   styleUrls: ['./project-search-page.component.css', '../../project-search.css'],
 })
-export class ProjectSearchPageComponent {
+export class ProjectSearchPageComponent implements OnInit {
   constructor(private projectReportService: ProjectReportService, private notificationService: NotificationService) { }
+
+  ngOnInit(): void {
+    // Use the RxJS fromEvent operator to handle the scroll event
+    fromEvent(this.reportsScrollableBox.nativeElement, 'scroll')
+      .subscribe(() => {
+        if (this.isScrolledToBottom()) {
+          if (this.nextPage && !this.isLoadingNextPage) {
+            this.loadNextPage();
+          }
+        }
+      });;
+  }
+
+  isScrolledToBottom(): boolean {
+    const container = this.reportsScrollableBox.nativeElement;
+    const atBottom = container.scrollTop + container.clientHeight + 10 >= container.scrollHeight;
+    return atBottom;
+  }
 
   form = new FormGroup({
     subcategory: new FormControl("DocumentInfo", Validators.required),
@@ -27,21 +45,28 @@ export class ProjectSearchPageComponent {
 
 
   onSearch() {
+    this.resetSearch();
     this.loadReports();
+  }
+
+  resetSearch() {
+    const container = this.reportsScrollableBox.nativeElement;
+    container.scrollTop = 0;
+    this.loadedReports = [];
+    this.nextPage = null;
+    this.lastLoadedPage = 1;
   }
 
   loadReports() {
     if (this.form.controls['value'].value?.length !== 0) {
-
       this.isLoading = true;
       this.projectReportService.getProjectReports(
         this.form.controls['subcategory'].value as string,
         this.form.controls['keyword'].value as string,
         this.form.controls['value'].value as string,
         1
-      ).pipe(delay(500)).subscribe(
+      ).subscribe(
         (response) => {
-          console.log(response)
           if (response.data.length == 0) {
             this.notificationService.displayMessage("No reports found.", "info");
           } else {
@@ -59,38 +84,22 @@ export class ProjectSearchPageComponent {
 
   // Scrollable window
   @ViewChild('reportsScrollableBox', { static: true }) reportsScrollableBox!: ElementRef;
-  @HostListener('scroll', ['$event'])
-  onScroll() {
-    const container = this.reportsScrollableBox!.nativeElement;
-
-    // Calculate the scroll position and check if it's at the bottom
-    const atBottom = container.scrollTop + container.clientHeight === container.scrollHeight;
-
-    if (atBottom) {
-      this.loadNextPage();
-    }
-  }
 
   loadNextPage() {
-    if (this.nextPage) {
-      console.log("Loading next page")
-      this.isLoadingNextPage = true;
-      this.projectReportService.getProjectReports(
-        this.form.controls['subcategory'].value as string,
-        this.form.controls['keyword'].value as string,
-        this.form.controls['value'].value as string,
-        (this.lastLoadedPage + 1)
-      ).subscribe(res => {
-        this.lastLoadedPage = res.pageNumber;
-        this.nextPage = res.nextPage;
-        for (let report of res.data) {
-          this.loadedReports.push(report);
-        }
-        this.isLoadingNextPage = false;
-      })
-
-    } else {
-      console.log("Last page loaded")
-    }
+    console.log("Loading next page")
+    this.isLoadingNextPage = true;
+    this.projectReportService.getProjectReports(
+      this.form.controls['subcategory'].value as string,
+      this.form.controls['keyword'].value as string,
+      this.form.controls['value'].value as string,
+      (this.lastLoadedPage + 1)
+    ).subscribe(res => {
+      this.lastLoadedPage = res.pageNumber;
+      this.nextPage = res.nextPage;
+      for (let report of res.data) {
+        this.loadedReports.push(report);
+      }
+      this.isLoadingNextPage = false;
+    })
   }
 }
