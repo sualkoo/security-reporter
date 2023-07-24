39c64a0e4a7075898b2e87a85ea403cb9314ad10
Benjamin Rahjou <rahjoubenjamin@gmail.com>
2023-07-24 10:53:13 +0000

    Update ICosmosService.cs GetItems List

--- SecurityReporter/webapi/Service/ICosmosService.cs
+++ SecurityReporter/webapi/Service/ICosmosService.cs
@@ -1,4 +1,6 @@
-﻿using webapi.Models;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using webapi.Models;
 using webapi.ProjectSearch.Models;
 
 namespace webapi.Service
@@ -9,6 +11,7 @@ namespace webapi.Service
         Task<bool> AddProjectReport(ProjectReportData data);
         Task<bool> DeleteProject(string projectId);
         Task<List<string>> DeleteProjects(List<string> projectIds);
+        Task<List<ProjectData>> GetItems(); // check
         Task<List<ProjectData>> GetItems(int pageSize, int pageNumber, FilterData filter);
         Task<int> GetNumberOfProjects();
     }
