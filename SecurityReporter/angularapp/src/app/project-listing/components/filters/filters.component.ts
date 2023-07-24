06a0692bf66c4da07ee4398c721df72e839e3603
Kamila Habalova <kamilahabalova747@gmail.com>
2023-07-24 13:55:08 +0200

    Add selectable items

--- SecurityReporter/angularapp/src/app/project-listing/components/filters/filters.component.ts
+++ SecurityReporter/angularapp/src/app/project-listing/components/filters/filters.component.ts
@@ -3,8 +3,7 @@ import { InputComponentComponent } from '../../../project-management/components/
 import { SelectComponentComponent } from '../../../project-management/components/select-component/select-component.component';
 import { FiltersDatepickerComponent } from '../datepicker/datepicker.component';
 import { SliderComponent } from '../slider/slider.component';
-
-
+import { SelectInterface } from '../../../project-management/interfaces/select-interface';
 
 @Component({
   selector: 'app-filters',
@@ -15,4 +14,35 @@ import { SliderComponent } from '../slider/slider.component';
 })
 export class FiltersComponent {
 
+  ProjectStatus: SelectInterface[] = [
+    { value: 'Requested', viewValue: 'Requested' },
+    { value: 'Planned', viewValue: 'Planned' },
+    { value: 'In progress', viewValue: 'In progress' },
+    { value: 'Finished', viewValue: 'Finished' },
+    { value: 'Cancelled', viewValue: 'Cancelled' },
+    { value: 'On hold', viewValue: 'On hold' },
+  ];
+
+  ProjectScope: SelectInterface[] = [
+    { value: 'TBS', viewValue: 'TBS' },
+    { value: 'Sent', viewValue: 'Sent' },
+    { value: 'Confirmed', viewValue: 'Confirmed' },
+    { value: 'Signed', viewValue: 'Signed' },
+  ];
+
+  Questionare: SelectInterface[] = [
+    { value: 'TBS', viewValue: 'TBS' },
+    { value: 'Sent', viewValue: 'Sent' },
+    { value: 'Received', viewValue: 'Received' },
+  ];
+
+  IKO: SelectInterface[] = [
+    { value: 'TBS', viewValue: 'TBS' },
+    { value: 'IKO', viewValue: 'IKO' },
+  ];
+
+  TKO: SelectInterface[] = [
+    { value: 'TBS', viewValue: 'TBS' },
+    { value: 'TKO', viewValue: 'TKO' },
+  ];
 }
