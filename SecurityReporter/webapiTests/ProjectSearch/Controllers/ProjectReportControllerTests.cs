01ba549a9bc4732a0d3b08733e198f30f072a3f5
Adrian Ihring <adrian.ihring@gmail.com>
2023-07-27 14:28:10 +0200

    Added unit tests for ProjectReportControllerTests

--- SecurityReporter/webapiTests/ProjectSearch/Controllers/ProjectReportControllerTests.cs
+++ SecurityReporter/webapiTests/ProjectSearch/Controllers/ProjectReportControllerTests.cs
@@ -14,60 +14,6 @@ using System.IO.Compression;
 using NUnit.Framework.Constraints;
 using Microsoft.AspNetCore.Http.HttpResults;
 
-
-// namespace webapi.ProjectSearch.Controllers.Tests
-// {
-//     [TestFixture()]
-//     public class ProjectReportControllerTests
-//     {
-//         // Helper method to create a stream from a string
-//         private static Stream GetStreamFromString(string content)
-//         {
-//             var stream = new MemoryStream();
-//             var writer = new StreamWriter(stream);
-//             writer.Write(content);
-//             writer.Flush();
-//             stream.Position = 0;
-//             return stream;
-//         }
-
-//         [Test]
-//         public async Task AddProjectReport_ValidFile_ReturnsOkResult()
-//         {
-//             // Arrange
-//             var mockProjectReportService = new Mock<IProjectReportService>();
-//             var mockLogger = new Mock<ILoggerFactory>();
-//             var mockLoggerInstance = new Mock<ILogger<ProjectReportController>>();
-
-//             // Replace with your expected ProjectReportData instance
-//             var expectedReportData = new ProjectReportData();
-
-//             mockProjectReportService.Setup(s => s.SaveReportFromZip(It.IsAny<IFormFile>()))
-//                                     .ReturnsAsync(expectedReportData);
-
-//             // Setup the mock ILoggerFactory to return the mock ILogger instance
-//             mockLogger.Setup(factory => factory.CreateLogger(It.IsAny<string>())).Returns(mockLoggerInstance.Object);
-
-//             var controller = new ProjectReportController(mockLogger.Object, mockProjectReportService.Object);
-
-//             // Create a mock form file using a stream
-//             var formFileContent = "Mock file content"; // Replace with your file content
-//             var formFile = new Mock<IFormFile>();
-//             formFile.Setup(f => f.OpenReadStream()).Returns(GetStreamFromString(formFileContent));
-//             formFile.Setup(f => f.Length).Returns(formFileContent.Length);
-//             formFile.Setup(f => f.FileName).Returns("mock-file.zip"); // Replace with your desired file name
-
-//             // Act
-//             var result = await controller.addProjectReport(formFile.Object) as OkObjectResult;
-
-//             // Assert
-//             Assert.IsNotNull(result);
-//             Assert.AreEqual(200, result.StatusCode);
-//             Assert.AreEqual(expectedReportData, result.Value);
-//         }
-//     }
-// }
-
 namespace webapi.ProjectReportControllers.Tests
 {
     [TestFixture()]
@@ -86,52 +32,68 @@ namespace webapi.ProjectReportControllers.Tests
         }
 
         [Test]
-        public void ProjectReportControllerTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void addProjectReportTest()
+        public void addProjectReport_ValidFile_ReturnsOkResult()
         {
             // Arrange
-
-            // Mock the CosmosService's behavior to return the expected project report
-
-
-            // Act
-
-
-            // Assert
-
-            Assert.Fail();
-        }
-
-        [Test]
-        public async Task addProjectReport_ValidFile_ReturnsOkResult()
-        {
-            // Arrange
-            IFormFile formFile = new FormFile(Stream.Null, 0, 0, "file", "jpg");
+            IFormFile formFile = new FormFile(Stream.Null, 0, 0, "file", "zip");
             ProjectReportData data = new ProjectReportData();
             data.Id = Guid.NewGuid();
 
-
-            //mockProjectReportService.Setup(service => service.SaveReportFromZip(formFile)).ReturnsAsync(data);
             mockProjectReportService.Setup(service => service.SaveReportFromZip(formFile)).ReturnsAsync(data);
 
             // Act
-            var result = await projectReportController.addProjectReport(formFile);
+            var result = projectReportController.addProjectReport(formFile).Result;
 
             // Assert
             Assert.IsNotNull(result);
-            Assert.IsTrue(result is ObjectResult objectResult && objectResult.StatusCode == 200);
-
-
-         
+            Assert.AreEqual(typeof(OkObjectResult), result.GetType());
+            Assert.IsTrue(result is ObjectResult objectResult && objectResult.StatusCode == 200);     
         }
 
         [Test]
-        public void getProjectReportByIdTest()
+        public void addProjectReport_InvalidFile_ReturnsNotAcceptableResult()
+        {
+            // Arrange
+            IFormFile formFile = new FormFile(Stream.Null, 0, 0, "file", "pdf");
+            ProjectReportData data = new ProjectReportData();
+            data.Id = Guid.NewGuid();
+
+            CustomException expectedException = new CustomException(StatusCodes.Status406NotAcceptable, "Invalid file type. Only .zip files are allowed.");
+
+            mockProjectReportService.Setup(service => service.SaveReportFromZip(formFile)).ThrowsAsync(expectedException);
+
+            // Act
+            var result = projectReportController.addProjectReport(formFile).Result;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(typeof(ObjectResult), result.GetType());
+            Assert.IsTrue(result is ObjectResult objectResult && objectResult.StatusCode == 406);
+        }
+
+        [Test]
+        public void addProjectReport_InvalidFile_ReturnsInternalServerErrorResult()
+        {
+            // Arrange
+            IFormFile formFile = new FormFile(Stream.Null, 0, 0, "file", "zip");
+            ProjectReportData data = new ProjectReportData();
+            data.Id = Guid.NewGuid();
+
+            CustomException expectedException = new CustomException(StatusCodes.Status500InternalServerError, "Failed to save ProjectReport to database.");
+
+            mockProjectReportService.Setup(service => service.SaveReportFromZip(formFile)).ThrowsAsync(expectedException);
+
+            // Act
+            var result = projectReportController.addProjectReport(formFile).Result;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(typeof(ObjectResult), result.GetType());
+            Assert.IsTrue(result is ObjectResult objectResult && objectResult.StatusCode == 500);
+        }
+
+        [Test]
+        public void getProjectReportByIdTest_ValidId_ReturnsOkResult()
         {
             // Arange
             Guid expectedId = Guid.NewGuid();
@@ -141,17 +103,16 @@ namespace webapi.ProjectReportControllers.Tests
             mockProjectReportService.Setup(service => service.GetReportByIdAsync(expectedId)).ReturnsAsync(data);
 
             // Act
-            var result = projectReportController.getProjectReportById(expectedId);
-            var resultResponse = result.Result;
+            var result = projectReportController.getProjectReportById(expectedId).Result;
 
             // Assert
             Assert.IsNotNull(result);
-            Assert.AreEqual(typeof(OkObjectResult), resultResponse.GetType());
-
+            Assert.AreEqual(typeof(OkObjectResult), result.GetType());
+            Assert.IsTrue(result is ObjectResult objectResult && objectResult.StatusCode == 200);
         }
 
         [Test]
-        public async void getProjectReportById_InvalidId_ReturnsErrorResponse()
+        public void getProjectReportById_InvalidId_ReturnsErrorResponse()
         {
             // Arange
             Guid expectedId = Guid.NewGuid();
@@ -160,18 +121,67 @@ namespace webapi.ProjectReportControllers.Tests
             mockProjectReportService.Setup(service => service.GetReportByIdAsync(expectedId)).ThrowsAsync(expectedException);
 
             // Act
-            var result = await projectReportController.getProjectReportById(expectedId);
+            var result = projectReportController.getProjectReportById(expectedId).Result;
 
             // Assert
             Assert.IsNotNull(result);
-            Assert.AreEqual(typeof(ErrorResponse), result.GetType());
+            Assert.AreEqual(typeof(ObjectResult), result.GetType());
+            Assert.IsTrue(result is ObjectResult objectResult && objectResult.StatusCode == 404);
         }
 
-        //  TODO
         [Test]
-        public void getProjectReportsAsyncTest()
+        public void getProjectReportFindings_ValidFinding_ReturnsOkResult()
         {
-            Assert.Fail();
+            // Arrange
+            string projectName = "Dummy Project 1";
+            string details = "The \\texttt{dummyapplication.apk} is signed with a debug certificate.";
+            string impact = "Debug certificates do not meet security standards of the release certificates.";
+            string repeatability = "The \\texttt{dummyapplication.apk} is signed with a debug certificate (\\texttt{CERT.RSA}), which can be found in the \\texttt{META-INF} folder.The certificate properties are shown in \\cref{figure:DebugCert}.";
+            string references = " \\href{https://doku-center.med.siemens.de/regelwerke/L4U-Intranet/GD/GD-41/GD-41-03-E.pdf}{Siemens Healthineers Guidance for Secure Software Architecture, Design andDevelopment: 8.4 Code-Signing}";
+            string cWE = "328";
+
+            string searchedValue = "Dumm";
+            int page = 1;
+
+            var expectedFinding = new FindingResponse();
+            expectedFinding.ProjectReportId = Guid.NewGuid();
+            expectedFinding.ProjectReportName = projectName;
+
+            var expectedData = new List<FindingResponse>
+            {
+                expectedFinding
+            };
+
+            var expectedResponse = new PagedDBResults<List<FindingResponse>>(expectedData, 1);
+
+            mockProjectReportService.Setup(service => service.GetReportFindingsAsync(projectName, details, impact, repeatability, references, cWE, searchedValue, page)).ReturnsAsync(expectedResponse);
+
+            // Act
+            var result = projectReportController.getProjectReportFindings(projectName, details, impact, repeatability, references, cWE, searchedValue, page).Result;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(typeof(OkObjectResult), result.GetType());
+            Assert.IsTrue(result is ObjectResult objectResult && objectResult.StatusCode == 200);
+        }
+
+        [Test]
+        public void getProjectReportFindings_InValidFinding_BadRequest()
+        {
+            // Arrange
+            string searchedValue = "Dummy";
+            int page = 1;
+
+            CustomException expectedException = new CustomException(StatusCodes.Status400BadRequest, "At list one filter has to be selected");
+
+            mockProjectReportService.Setup(service => service.GetReportFindingsAsync(null, null, null, null, null, null, searchedValue, page)).ThrowsAsync(expectedException);
+
+            // Act
+            var result = projectReportController.getProjectReportFindings(null, null, null, null, null, null, searchedValue, page).Result;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result is ObjectResult objectResult && objectResult.StatusCode == 400);
         }
     }
 }
