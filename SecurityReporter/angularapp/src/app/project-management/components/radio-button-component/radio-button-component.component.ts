e923300a30c9e61f1a4a382aff50d68c423d391b
Martin Kubička <martinkubicka22@gmail.com>
2023-08-01 17:41:49 +0200

    fill radio button

--- SecurityReporter/angularapp/src/app/project-management/components/radio-button-component/radio-button-component.component.ts
+++ SecurityReporter/angularapp/src/app/project-management/components/radio-button-component/radio-button-component.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { MatRadioModule } from '@angular/material/radio';
 import { FormsModule } from '@angular/forms';
+import { ProjectInterface } from '../../interfaces/project-interface';
 
 @Component({
   selector: 'app-radio-button-component',
@@ -9,18 +10,48 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [MatRadioModule, FormsModule],
 })
-export class RadioButtonComponentComponent {
+export class RadioButtonComponentComponent implements OnInit {
   @Input() title: string = '';
-  radioValue: number = 0;
-  inputValue: number = 5;
+  @Input() project: ProjectInterface | undefined = undefined;
+  radioValue: string = '0';
+  inputValue: number | undefined = 5;
 
   @Output() valueChanged = new EventEmitter<number>();
 
+  async ngOnInit() {
+    await this.waitForProjectName();
+
+    if (this.project) {
+      if (this.project.PentestDuration == 2) {
+        this.radioValue = '2';
+      } else if (this.project.PentestDuration == 4) {
+        this.radioValue = '4';
+      } else {
+        this.radioValue = '3';
+        this.inputValue = this.project.PentestDuration;
+      }
+    }
+  }
+
+  waitForProjectName(): Promise<void> {
+    return new Promise<void>((resolve) => {
+      const checkProjectName = () => {
+        if (this.project && this.project.ProjectName !== '') {
+          resolve();
+        } else {
+          setTimeout(checkProjectName, 100);
+        }
+      };
+
+      checkProjectName();
+    });
+  }
+
   onRadioChange() {
-    if (this.radioValue == 3) {
+    if (parseInt(this.radioValue) == 3) {
       this.valueChanged.emit(this.inputValue);
     } else {
-      this.valueChanged.emit(this.radioValue);
+      this.valueChanged.emit(parseInt(this.radioValue));
     }
   }
 }
