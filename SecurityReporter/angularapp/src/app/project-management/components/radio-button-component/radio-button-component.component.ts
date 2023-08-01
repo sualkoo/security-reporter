d7543fc2d7e7a71032c53ef6f1f87724a6e77d92
[Petra Mikova] <61084407+PetraMikova@users.noreply.github.com>
2023-08-01 09:17:12 +0200

    Add validation to input field of pentest duration and reoslve problems with its old enum structure

--- SecurityReporter/angularapp/src/app/project-management/components/radio-button-component/radio-button-component.component.ts
+++ SecurityReporter/angularapp/src/app/project-management/components/radio-button-component/radio-button-component.component.ts
@@ -1,18 +1,21 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, NgModule } from '@angular/core';
 import { MatRadioModule } from '@angular/material/radio';
+import { CommonModule, NgIf } from '@angular/common'
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-radio-button-component',
   templateUrl: './radio-button-component.component.html',
   styleUrls: ['./radio-button-component.component.css'],
   standalone: true,
-  imports: [MatRadioModule, FormsModule],
+  imports: [MatRadioModule, FormsModule, CommonModule, MatInputModule],
 })
 export class RadioButtonComponentComponent {
   @Input() title: string = '';
   radioValue: number = 0;
   inputValue: number = 5;
+  selectedOption: number = 0;
 
   @Output() valueChanged = new EventEmitter<number>();
 
