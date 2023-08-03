449c25cf467d711f75c20e07cd03a3d462001a85
Martin Lipták <12054222-katpil320@users.noreply.gitlab.com>
2023-08-03 13:25:54 +0200

    Added testing methodology extractor

--- SecurityReporter/webapi/ProjectSearch/Services/Extractor/DBToZipExtract/DBTestingMethodologyExtractor.cs
+++ SecurityReporter/webapi/ProjectSearch/Services/Extractor/DBToZipExtract/DBTestingMethodologyExtractor.cs
@@ -1,17 +1,58 @@
-﻿using webapi.Models.ProjectReport;
+﻿using System.Text;
+using webapi.Models.ProjectReport;
 
 namespace webapi.ProjectSearch.Services.Extractor.DBToZipExtract
 {
     public class DBTestingMethodologyExtractor
     {
-        private TestingMethodology testingMethodology = null;
-        public DBTestingMethodologyExtractor(TestingMethodology testingMethodology)
+        public byte[] extractTestingMethodology(TestingMethodology testingMethodology)
         {
-            this.testingMethodology = testingMethodology;
-        }
-        public byte[] extractTestingMethodology()
-        {
-            return null;
+
+            var toolsUsed = new List<string>();
+            if (testingMethodology.ToolsUsed != null) {
+                foreach(Tool tool in testingMethodology.ToolsUsed)
+                {
+                    toolsUsed.Add("\t" + tool.ToolName + " & " + tool.Version + " & " + tool.TestType + " & " + tool.WorkType + " \\\\\n\t\\hline");
+                }
+            }
+
+            var attackVectors = new List<string>();
+            if (testingMethodology.AttackVectors != null)
+            {
+                foreach(string attackVector in testingMethodology.AttackVectors)
+                {
+                    attackVectors.Add("\t\t\\item " + attackVector);
+                }
+            }
+
+            string testingMethodologyContent =
+@"%----------------------------------------------------------------------------------------
+%	TOOLS USED
+%----------------------------------------------------------------------------------------
+% Not needed for Scope document
+% Required for Report document
+\newcommand{\ToolsUsed}{
+	\hline 
+" + string.Join("\n", toolsUsed) + @"
+} 
+
+
+%----------------------------------------------------------------------------------------
+%	ATTACK VECTORS AND PAYLOAD TYPES
+%----------------------------------------------------------------------------------------
+% Not needed for Scope document
+% Required for Report document
+\newcommand{\AttackVectors}{
+
+	Tests on \ReportProjectName included, but were not limited to:
+
+	\begin{itemize}
+" + string.Join(",\n" ,attackVectors) + @"
+	\end{itemize}
+
+}";
+            Console.WriteLine(testingMethodologyContent);
+            return Encoding.UTF8.GetBytes(testingMethodologyContent);
         }
     }
 }
---
+++ SecurityReporter/webapiTests/ProjectSearch/Services/Extractor/DBToZipExtract/DBTestingMethodologyExtractorTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using System.Text;
+using webapi.Models.ProjectReport;
+
+namespace webapi.ProjectSearch.Services.Extractor.DBToZipExtract.Tests
+{
+    [TestFixture()]
+    public class DBTestingMethodologyExtractorTests
+    {
+        [Test()]
+        public void extractTestingMethodologyTest()
+        {
+            // Arrange
+            DBTestingMethodologyExtractor extractor = new DBTestingMethodologyExtractor();
+            TestingMethodology testingMethodology = new TestingMethodology();
+
+            var t = new Tool();
+            t.ToolName = "adb";
+            t.Version = "1.0.41";
+            t.TestType = "Android debugging";
+            t.WorkType = "Bridge to Andorid device";
+
+            var t1 = new Tool();
+            t1.ToolName = "Android Studio";
+            t1.Version = "2022.2.1";
+            t1.TestType = "Android Development, Emulator";
+            t1.WorkType = "Official integrated development environment for Google's Android operating system, with emulator capabilities.";
+
+            testingMethodology.ToolsUsed = new List<Tool>() 
+            {
+                t,
+                t1
+            };
+
+            testingMethodology.AttackVectors = new List<string>()
+            {
+                "static analysis",
+                "file system analysis",
+                "debugging",
+                "workflow analysis",
+                "client-side testing",
+                "testing for weak cryptography",
+                "testing error handling."
+            };
+
+            // Act
+            var result = extractor.extractTestingMethodology(testingMethodology);
+            var resultDecoded = Encoding.UTF8.GetString(result);
+
+            // Assert
+            Assert.IsNotNull(resultDecoded);
+
+            StringAssert.Contains("%----------------------------------------------------------------------------------------\r\n%\tTOOLS USED\r\n%----------------------------------------------------------------------------------------\r\n% Not needed for Scope document\r\n% Required for Report document", resultDecoded);
+            StringAssert.Contains("\\newcommand{\\ToolsUsed}{", resultDecoded);
+            StringAssert.Contains("\t\\hline", resultDecoded);
+            StringAssert.Contains("\tadb & 1.0.41 & Android debugging & Bridge to Andorid device \\\\", resultDecoded);
+            StringAssert.Contains("\tAndroid Studio & 2022.2.1 & Android Development, Emulator & Official integrated development environment for Google's Android operating system, with emulator capabilities. \\\\", resultDecoded);
+            StringAssert.Contains("%----------------------------------------------------------------------------------------\r\n%\tATTACK VECTORS AND PAYLOAD TYPES\r\n%----------------------------------------------------------------------------------------\r\n% Not needed for Scope document\r\n% Required for Report document", resultDecoded);
+            StringAssert.Contains("\\newcommand{\\AttackVectors}{", resultDecoded);
+            StringAssert.Contains("Tests on \\ReportProjectName included, but were not limited to:", resultDecoded);
+            StringAssert.Contains("\begin{itemize}", resultDecoded);
+            StringAssert.Contains("\t\t\\item static analysis,", resultDecoded);
+            StringAssert.Contains("\t\t\\item file system analysis,", resultDecoded);
+            StringAssert.Contains("\t\t\\item debugging,", resultDecoded);
+            StringAssert.Contains("\t\t\\item workflow analysis,", resultDecoded);
+            StringAssert.Contains("\t\t\\item client-side testing,", resultDecoded);
+            StringAssert.Contains("\t\t\\item testing for weak cryptography,", resultDecoded);
+            StringAssert.Contains("\t\t\\item testing error handling.", resultDecoded);
+            StringAssert.Contains("\t\\end{itemize}\r\n\r\n}", resultDecoded);
+        }
+    }
+}
