5f0506b7e7eba62f6d667b6f2e22b1392e00085b
Radoslav Žilka <radoslavZilka@outlook.com>
2023-08-03 14:26:24 +0200

    Added entry point for extracting data from database

---
+++ SecurityReporter/webapi/ProjectSearch/Services/Extractor/DBProjectDataParser.cs
@@ -0,0 +1,14 @@
+﻿using System.IO.Compression;
+using webapi.ProjectSearch.Models;
+
+namespace webapi.ProjectSearch.Services.Extractor
+{
+    public class DBProjectDataParser : IDBProjectDataParser
+    {
+        private ProjectReportData projectReportData;
+        public byte[] Extract(ProjectReportData projectReportData)
+        {
+            this.projectReportData = projectReportData;
+        }
+    }
+}
---
+++ SecurityReporter/webapi/ProjectSearch/Services/Extractor/IDBProjectDataParser.cs
@@ -0,0 +1,9 @@
+﻿using webapi.ProjectSearch.Models;
+
+namespace webapi.ProjectSearch.Services.Extractor
+{
+    public interface IDBProjectDataParser
+    {
+        public byte[] Extract(ProjectReportData projectReportData);
+    }
+}
