1fbc6ee9cc8d4f2d69b9a3d09e416d8243c724f0
Martin Šponiar <sponiarmartin2000@gmail.com>
2023-08-09 13:17:48 +0200

    Config creation fixed

--- SecurityReporter/angularapp/src/app/dashboard/providers/graph-config.ts
+++ SecurityReporter/angularapp/src/app/dashboard/providers/graph-config.ts
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+export const CriticalityConfig = {
+  type: 'pie',
+  label: '% of found Criticality',
+  backgroundColors: [
+    'rgb(217,0,45)',
+    'rgb(0,151,255)',
+    'rgb(253,179,0)',
+    'rgb(102,255,0)',
+    'rgb(153,102,255)',
+  ],
+  borderWidth: 0,
+};
