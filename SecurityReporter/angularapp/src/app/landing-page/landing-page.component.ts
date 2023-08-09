e68a3cd0b196d86dcc43fd987cbd8ff20ff9e9ab
Samuel Popjakel <samuelpopjak@outlook.sk>
2023-08-09 09:27:41 +0200

    Frontend landing page

--- SecurityReporter/angularapp/src/app/landing-page/landing-page.component.ts
+++ SecurityReporter/angularapp/src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css', '../project-search.css'],
 })
 export class LandingPageComponent {
+  constructor(private router: Router) { }
+  navigateToPage(page: string): void {
+    this.router.navigate([page]);
+  }
 }
