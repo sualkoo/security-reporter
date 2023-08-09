d2104cb20b06daf2771065e3f26487a1b37e697e
Martin Kubička <martinkubicka22@gmail.com>
2023-08-09 08:37:15 +0200

    add try catch

--- SecurityReporter/webapi/Login/Services/RoleService.cs
+++ SecurityReporter/webapi/Login/Services/RoleService.cs
@@ -19,11 +19,15 @@ namespace webapi.Login.Services
         {
 
             // REMOVE ME IN PRODUCTION VERSION
-            cosmosRolesService.AddRole("client@client.sk", "client");
-            cosmosRolesService.AddRole("admin@admin.sk", "admin");
-            cosmosRolesService.AddRole("pentester@pentester.sk", "pentester");
-            cosmosRolesService.AddRole("coordinator@coordinator.sk", "coordinator");
-            cosmosRolesService.AddRole("default@default.sk", "default");
+            try
+            {
+                cosmosRolesService.AddRole("client@client.sk", "client");
+                cosmosRolesService.AddRole("admin@admin.sk", "admin");
+                cosmosRolesService.AddRole("pentester@pentester.sk", "pentester");
+                cosmosRolesService.AddRole("coordinator@coordinator.sk", "coordinator");
+                cosmosRolesService.AddRole("default@default.sk", "default");
+            }
+            catch { }
 
 
             var mail = clientMailService.GetClientMail(subjectId);
