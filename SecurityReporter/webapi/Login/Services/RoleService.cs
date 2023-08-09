647289bf67ae83eba9a5307c94e5bd939989caf9
Martin Kubička <martinkubicka22@gmail.com>
2023-08-09 14:34:13 +0200

    fix getting count of items when client is logged in

--- SecurityReporter/webapi/Login/Services/RoleService.cs
+++ SecurityReporter/webapi/Login/Services/RoleService.cs
@@ -17,19 +17,6 @@ namespace webapi.Login.Services
         }
         public async Task<string> GetUserRoleBySubjectId(string subjectId)
         {
-
-            // REMOVE ME IN PRODUCTION VERSION
-            try
-            {
-                cosmosRolesService.AddRole("client@client.sk", "client");
-                cosmosRolesService.AddRole("admin@admin.sk", "admin");
-                cosmosRolesService.AddRole("pentester@pentester.sk", "pentester");
-                cosmosRolesService.AddRole("coordinator@coordinator.sk", "coordinator");
-                cosmosRolesService.AddRole("default@default.sk", "default");
-            }
-            catch { }
-
-
             var mail = clientMailService.GetClientMail(subjectId);
 
             if (mail != null)
