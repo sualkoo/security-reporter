44905c6bca71ee16ce54fc268ddbbb682e259ade
Kevin Laurinčík <kevin.laurincik@gmail.com>
2023-08-18 11:06:50 +0200

    Remake of navbar depending on roles and log in state.

--- SecurityReporter/angularapp/src/app/navigation-bar/navigation-bar.component.ts
+++ SecurityReporter/angularapp/src/app/navigation-bar/navigation-bar.component.ts
@@ -12,11 +12,12 @@ export class NavigationBarComponent {
 
   @Input() public mail: string = "samuelpopjak@outlook.sk";
 
-  public showIntitials = false;
+  public showInitials = true;
   public initials: string = "";
   public role: string = "admin";
-  public isLoggedIn: boolean = true;
+  public isLoggedIn: boolean = false;
   public actualNavItem: string = "/project-search";
+  public circleColor: string = "slate-grey";
 
   setNavItem(name: string): void {
     this.actualNavItem = name;
@@ -24,7 +25,30 @@ export class NavigationBarComponent {
 
   ngOnInit() {
     this.createInitials();
+    this.setNavbar();
   }
+    setNavbar() {
+      if (!this.isLoggedIn) {
+        this.menuItems = this.defaultMenuItems;
+      } else {
+        switch (this.role) {
+          case "admin":
+            this.menuItems = this.adminMenuItems;
+            break;
+          case "pentester":
+            this.menuItems = this.pentesterMenuItems;
+            break;
+          case "client":
+            this.menuItems = this.clientMenuItems;
+            break;
+          case "coordinator":
+            this.menuItems = this.coordinatorMenuItems;
+            break;
+          default:
+            this.menuItems = this.defaultMenuItems;
+        }
+      }
+    }
 
 
   private createInitials(): void {
@@ -32,6 +56,45 @@ export class NavigationBarComponent {
   }
 
   login() {
-    this.isLoggedIn = !this.isLoggedIn;
+    this.isLoggedIn = true;
+    this.setNavbar();
   }
+
+  logout() {
+    this.isLoggedIn = false;
+    this.setNavbar();
+  }
+
+  menuItems: { text: string, link: string, disabled?: boolean }[] = [];
+
+  adminMenuItems: { text: string, link: string, disabled?: boolean }[] = [
+    { text: 'Home', link: '/welcome', disabled: false },
+    { text: 'Project Search', link: '/project-search', disabled: false },
+    { text: 'Project Management', link: '/project-management', disabled: false },
+    { text: 'Dashboard', link: '/dashboard', disabled: false },
+  ];
+
+  defaultMenuItems: { text: string, link: string, disabled?: boolean }[] = [
+    { text: 'Home', link: '/welcome', disabled: false },
+    { text: 'About pentest', link: '/about-pentests', disabled: false },
+    { text: 'Order a pentest', link: '/in-development', disabled: true },
+  ];
+
+  pentesterMenuItems: { text: string, link: string, disabled?: boolean }[] = [
+    { text: 'Home', link: '/welcome', disabled: false },
+    { text: 'About pentest', link: '/about-pentests', disabled: false },
+    { text: 'Order a pentest', link: '/in-development', disabled: true },
+  ];
+
+  coordinatorMenuItems: { text: string, link: string, disabled?: boolean }[] = [
+    { text: 'Home', link: '/welcome', disabled: false },
+    { text: 'About pentest', link: '/about-pentests', disabled: false },
+    { text: 'Order a pentest', link: '/in-development', disabled: true },
+  ];
+
+  clientMenuItems: { text: string, link: string, disabled?: boolean }[] = [
+    { text: 'Home', link: '/welcome', disabled: false },
+    { text: 'About pentest', link: '/about-pentests', disabled: false },
+    { text: 'Order a pentest', link: '/in-development', disabled: true },
+  ];
 }
