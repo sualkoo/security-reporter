2539eb7bf87ec3ee51497a6e3cf89bf57448512e
Martin Laus <mato.laus33@gmail.com>
2023-08-24 11:44:04 +0200

    Add sign out functionality

--- SecurityReporter/angularapp/src/app/my-profile/component-pages/my-profile-page/my-profile.component.ts
+++ SecurityReporter/angularapp/src/app/my-profile/component-pages/my-profile-page/my-profile.component.ts
@@ -4,6 +4,10 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { BacklogComponentComponent } from '../../components/backlog-component/backlog-component.component';
 import { GetRoleService } from 'src/app/shared/services/get-role.service';
 import { GetEmailService } from 'src/app/shared/services/get-email.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LogoutService } from '../../../services/logout.service';
+import { AlertService } from '../../../project-management/services/alert.service';
 
 @Component({
   selector: 'app-my-profile',
@@ -18,7 +22,11 @@ export class MyProfileComponent {
 
   constructor(
     private getRoleService: GetRoleService,
-    private getEmailService: GetEmailService
+    private getEmailService: GetEmailService,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    private logOut: LogoutService,
+    private alertService: AlertService
   ) {
     this.getRole();
     this.getEmail();
@@ -30,5 +38,31 @@ export class MyProfileComponent {
 
   async getEmail() {
     this.email = await this.getEmailService.getEmail();
+  }
+
+  logoutPage() {
+    if (this.logOut.logout()) {
+      this.alertService.showSnackbar(
+        'You are being logged out',
+        'Close',
+        'red-alert',
+        5000
+      );
+    } else {
+      this.alertService.showSnackbar(
+        'Error happened during logout',
+        'Close',
+        'red-alert',
+        5000
+      );
+    }
+
+    this.snackBar.open('You are being logged out.', 'Close', {
+      duration: 5000,
+      panelClass: 'red-alert',
+    });
+    setTimeout(() => {
+      window.location.href = '/welcome';
+    }, 1000);
   }
 }
